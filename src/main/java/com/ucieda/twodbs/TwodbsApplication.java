eba4ef4e6dbd82837e1b816ae5fd8cb9e9d7980d
Wilder Ucieda <wilder.ucieda@gmail.com>
2024-10-30 15:28:08 +0100

    Initial commit

---
+++ src/main/java/com/ucieda/twodbs/TwodbsApplication.java
@@ -0,0 +1,13 @@
+package com.ucieda.twodbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwodbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwodbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ucieda/twodbs/TwodbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucieda.twodbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwodbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
